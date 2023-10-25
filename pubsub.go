920ed9d5b96283f787905ca49453373c1485afeb
Alex Bowes <alex.bowes@blueambertech.co.uk>
2023-10-25 21:52:31 +0100

    Added push definition

--- pubsub.go
+++ pubsub.go
@@ -7,4 +7,5 @@ import (
 
 type Handler interface {
 	Subscribe(cancCtx context.Context, subID string, refreshRate time.Duration, msgProc func(c context.Context, msgData []byte))
+	Push(ctx context.Context, topicID, msg string) error
 }
